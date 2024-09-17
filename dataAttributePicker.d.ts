3f0797d9355c6d9c4a433865e2d0a6721bccd4da
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2024-09-17 13:59:48 +0200

    Updates

--- dataAttributePicker.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Tree } from '@openscd/oscd-tree-grid';
-export declare function dataAttributeTree(doc: XMLDocument): Tree;
-export declare function getSourceDef(paths: string[][]): string[];
